de2ece9e430a88e9174db1488bb7f809e433d984
Pavel Chernyshev <vengador32@gmail.com>
2019-11-06 16:15:44 +0600

    added Sendex snippet

--- core/components/sendex/elements/snippets/snippet.sendex.php
+++ core/components/sendex/elements/snippets/snippet.sendex.php
@@ -1,7 +1,36 @@
 <?php
+/** @var array $scriptProperties */
+/** @var Sendex $Sendex */
 
 $Sendex = $modx->getService('sendex','Sendex',$modx->getOption('sendex_core_path',null,$modx->getOption('core_path').'components/sendex/').'model/sendex/',$scriptProperties);
 if (!($Sendex instanceof Sendex)) return '';
+
+/** @var pdoTools $pdoTools */
+$pdoTools = $modx->getService('pdoTools');
+
+if (!($Sendex instanceof Sendex) || !($pdoTools instanceof pdoTools)) return '';
+
+if (!$modx->user->isAuthenticated($modx->context->key)) {
+	return $modx->lexicon('sendex_err_auth_req');
+} elseif (empty($id) || !$newsletter = $modx->getObject('sxNewsletter', $id)) {
+	return $modx->lexicon('sendex_newsletter_err_ns');
+}
+
+/** @var sxNewsletter $newsletter */
+if (!$newsletter->active && empty($showInactive)) {
+	return $modx->lexicon('sendex_newsletter_err_disabled');
+}
+
+if ($newsletter->isSubscribed($modx->user->id)) {
+	return !empty($tplUnsubscribeForm)
+	? $pdoTools->getChunk($tplUnsubscribeForm, $newsletter->toArray())
+	: 'Parameter "tplUnsubscribeForm" is empty';
+}
+elseif (!$newsletter->isSubscribed($modx->user->id)) {
+	return !empty($tplSubscribeForm)
+	? $pdoTools->getChunk($tplSubscribeForm, $newsletter->toArray())
+	: 'Parameter "tplSubscribeForm" is empty'; 
+}
 
 /**
  * Do your snippet code here. This demo grabs 5 items from our custom table.
