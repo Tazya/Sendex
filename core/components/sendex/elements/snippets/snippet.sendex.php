4906e13897bae2c6e093be392e5e4bcbe37f9700
Pavel Chernyshev <vengador32@gmail.com>
2019-11-06 17:00:57 +0600

    snippet Sendex with properties

--- core/components/sendex/elements/snippets/snippet.sendex.php
+++ core/components/sendex/elements/snippets/snippet.sendex.php
@@ -31,37 +31,3 @@ elseif (!$newsletter->isSubscribed($modx->user->id)) {
 	? $pdoTools->getChunk($tplSubscribeForm, $newsletter->toArray())
 	: 'Parameter "tplSubscribeForm" is empty'; 
 }
-
-/**
- * Do your snippet code here. This demo grabs 5 items from our custom table.
- */
-$tpl = $modx->getOption('tpl',$scriptProperties,'Item');
-$sortBy = $modx->getOption('sortBy',$scriptProperties,'name');
-$sortDir = $modx->getOption('sortDir',$scriptProperties,'ASC');
-$limit = $modx->getOption('limit',$scriptProperties,5);
-$outputSeparator = $modx->getOption('outputSeparator',$scriptProperties,"\n");
-
-/* build query */
-$c = $modx->newQuery('SendexItem');
-$c->sortby($sortBy,$sortDir);
-$c->limit($limit);
-$items = $modx->getCollection('SendexItem',$c);
-
-/* iterate through items */
-$list = array();
-/* @var SendexItem $item */
-foreach ($items as $item) {
-	$itemArray = $item->toArray();
-	$list[] = $Sendex->getChunk($tpl,$itemArray);
-}
-
-/* output */
-$output = implode($outputSeparator,$list);
-$toPlaceholder = $modx->getOption('toPlaceholder',$scriptProperties,false);
-if (!empty($toPlaceholder)) {
-	/* if using a placeholder, output nothing and set output to specified placeholder */
-	$modx->setPlaceholder($toPlaceholder,$output);
-	return '';
-}
-/* by default just return output */
-return $output;
